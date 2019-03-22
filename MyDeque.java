097c6a0ef14e323530d730fa9aec80cc45d3a38f
YeseulChoi0 <ychoi00@stuy.edu>
2019-03-22 06:29:39 -0400

    Started files

---
+++ MyDeque.java
@@ -0,0 +1,74 @@
+@SuppressWarnings("unchecked")
+public class MyDeque<E>{
+  private E[] data;
+  private int size, start, end;
+
+  public MyDeque(){
+    data = (E[])new Object[10];
+  }
+
+  public MyDeque(int initialCapacity){
+    data = (E[])new Object[initialCapacity];
+  }
+
+  public int size(){
+    return size;
+  }
+
+  public String toString(){
+    String s = "[";
+    for (int i =0; i < size; i++){
+      s += data[i] + ", ";
+    }
+    return s + "]";
+  }
+
+  public void addFirst(E element){
+    if (start == 0){
+      start = data.length - 1;
+    }else{
+      start--;
+    }
+    data[start] = element;
+    size++;
+  }
+
+  public void addLast(E element){
+    if (end == data.length-1){
+      end = 0;
+    }else{
+      end++;
+    }
+    data[end] = element;
+    size++;
+  }
+
+  public E removeFirst(){
+    if (start == data.length-1){
+      start = 0;
+    }else{
+      start++;
+    }
+    data[start] = null;
+    size--;
+  }
+
+  public E removeLast(){
+    if (end == 0){
+      end = data.length - 1;
+    }else{
+      end--;
+    }
+    data[end] = null;
+    size--;
+  }
+
+  public E getFirst(){
+    return data[start];
+  }
+
+  public E getLast(){
+    return data[end];
+  }
+
+}
